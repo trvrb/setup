e1bcdba276ab368d34b25b32ed23105711d54bfd
Trevor Bedford <trvrb@trevorbedford.com>
2014-03-22 18:53:03 -0700

    Workaround for Apple's recent changes to clang.

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ echo "Setup Ruby environment"
 rbenv install 2.1.1
 rbenv global 2.1.1
 gem install bundler
-bundle install
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future bundle install
 rbenv rehash
 
 echo "Setup Python environment"
